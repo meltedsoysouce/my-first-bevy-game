635943a23fb667cc3e423ad0ee223822583d4b5f
Mchi_yuki <metalsngke@gmail.com>
2024-09-17 07:07:25 +0900

    四角を表示する

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use bevy::{
-    prelude::*
+    prelude::*,
+    sprite::{Wireframe2dConfig, Wireframe2dPlugin, MaterialMesh2dBundle, Mesh2dHandle},
+    color::palettes::*
 };
 
 fn main() {
     App::new()
-        .add_systems(Startup, hello_world)
+        .add_plugins((DefaultPlugins, Wireframe2dPlugin))
+        .add_systems(Startup, setup)
         .run();
 }
 
-fn hello_world() {
-    println!("Hello, world!");
+fn setup(mut commands: Commands,
+         mut meshes: ResMut<Assets<Mesh>>,
+         mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+    let color = Color::srgb(0.3, 0.7, 0.5);
+
+    let shape = Mesh2dHandle(meshes.add(Rectangle::new(50., 100.)));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: shape,
+        material: materials.add(color),
+        transform: Transform::from_xyz(0., 0., 0.),
+        ..default()
+    });
 }
