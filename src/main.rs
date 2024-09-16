ceb30dc45ad47dab5c176f5c75eadbeaeff6d78c
Mchi_yuki <metalsngke@gmail.com>
2024-09-17 06:54:36 +0900

    コンソールに文字を表示するシステムを開発

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::{
+    prelude::*
+};
+
 fn main() {
+    App::new()
+        .add_systems(Startup, hello_world)
+        .run();
+}
+
+fn hello_world() {
     println!("Hello, world!");
 }
