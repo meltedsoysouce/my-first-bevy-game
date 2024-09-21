82f6f02ede88a659331b9c45ce4cb67dfe228690
Mchi_yuki <metalsngke@gmail.com>
2024-09-21 14:42:46 +0900

    space押下時に、プレイヤーの場所で弾が出現するように変更

--- src/bullet.rs
+++ src/bullet.rs
@@ -3,12 +3,15 @@ use bevy::{
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 
+use crate::player::Player;
+
 #[derive(Component)]
 pub struct Bullet;
 
 impl Plugin for Bullet {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, Bullet::setup);
+        app.add_systems(Startup, Bullet::setup)
+            .add_systems(Update, Bullet::update);
     }
 }
 
@@ -23,8 +26,29 @@ impl Bullet {
                 mesh: Mesh2dHandle(meshes.add(Rectangle::new(10., 10.))),
                 material: materials.add(Color::srgb(1., 1., 0.)),
                 transform: Transform::from_xyz(0., 0., 0.),
+                visibility: Visibility::Hidden,
                 ..default()
             })
             .insert(Bullet);
+    }
+
+    fn update(
+        input: Res<ButtonInput<KeyCode>>,
+        mut query: Query<(&mut Transform, &mut Visibility), With<Bullet>>,
+        player: Query<&Transform, (With<Player>, Without<Bullet>)>,
+    ) {
+        let player_t = player.single();
+        if input.pressed(KeyCode::Space) {
+            for (mut transform, mut visibility) in query.iter_mut() {
+                *visibility = Visibility::Visible;
+                transform.translation.x = player_t.translation.x;
+            }
+        } else {
+            for (_, mut visibility) in query.iter_mut() {
+                if *visibility == Visibility::Visible {
+                    *visibility = Visibility::Hidden;
+                }
+            }
+        }
     }
 }
