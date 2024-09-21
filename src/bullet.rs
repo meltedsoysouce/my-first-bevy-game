d133ee5c27d2d4e52200c7c8d7993ba595e4e0aa
Mchi_yuki <metalsngke@gmail.com>
2024-09-21 12:54:08 +0900

    弾の色を変更

--- src/bullet.rs
+++ src/bullet.rs
@@ -21,7 +21,7 @@ impl Bullet {
         commands
             .spawn(MaterialMesh2dBundle {
                 mesh: Mesh2dHandle(meshes.add(Rectangle::new(10., 10.))),
-                material: materials.add(Color::srgb(0.7, 0.3, 0.5)),
+                material: materials.add(Color::srgb(1., 1., 0.)),
                 transform: Transform::from_xyz(0., 0., 0.),
                 ..default()
             })
